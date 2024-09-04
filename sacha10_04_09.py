65a30196e487e2d4169bb124f156e8b0fa69dc5c
sacha10808 <sacha10sacha@gmail.com>
2024-09-05 00:03:06 +0300

    Add files via upload

---
+++ sacha10_04_09.py
@@ -0,0 +1,16 @@
+print (12+89-1)
+print ("1st program")
+print((9**0.5)*5)
+print ("2nd program")
+print((9.99>9.98), (1000!=1000.1))
+print ("3rd program")
+print((2*2+2),(2*(2+2)))
+print((2*2+2)==(2*(2+2)))
+print("4th program")
+print((int(10*float('123.456')))%10)
+
+
+
+
+
+
